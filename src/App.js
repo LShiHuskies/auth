02512e15b75e8e8ae9c34d80cb1447c3b560bd67
Louis Shi <Shilou2010@gmail.com>
2019-03-31 13:46:05 -0400

    loginform and appjs and firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+        apiKey: "AIzaSyDhyplVF90XTHBF_-WPMbYTY0JoNP5FumM",
+        authDomain: "authentication-117f6.firebaseapp.com",
+        databaseURL: "https://authentication-117f6.firebaseio.com",
+        projectId: "authentication-117f6",
+        storageBucket: "authentication-117f6.appspot.com",
+        messagingSenderId: "763277430974"
+      });
+  }
   render() {
     return (
       <View>
@@ -10,6 +23,7 @@ class App extends Component {
         <Text>
           An App
         </Text>
+        <LoginForm />
       </View>
     )
   }
